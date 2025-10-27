d82f3f4d881c924f964116575f274e2e61c54258
Laura <thibmorozier@live.nl>
2025-10-27 14:59:13 +0100

    Small GUI improvements

--- Packages/com.laurarozier.paramcomp/Editor/Components/ParamCompSettingsEditor.cs
+++ Packages/com.laurarozier.paramcomp/Editor/Components/ParamCompSettingsEditor.cs
@@ -8,24 +8,39 @@ namespace ParamComp.Editor.Components
     [CustomEditor(typeof(ParamCompSettings))]
     public class ParamCompSettingsEditor : UnityEditor.Editor
     {
+        // Parameter Name Exclusions
         private SerializedProperty _ExcludedPropertyNamesProperty;
         private SerializedProperty _ExcludedPropertyNamePrefixesProperty;
         private SerializedProperty _ExcludedPropertyNameSuffixesProperty;
+        // Package Specific Exclusions
         private SerializedProperty _ExcludeVRCFTProperty;
+        // Parameter Type Exclusions
         private SerializedProperty _ExcludeBoolsProperty;
         private SerializedProperty _ExcludeIntsProperty;
         private SerializedProperty _ExcludeFloatsProperty;
+        // Output Settings
         private SerializedProperty _BoolsPerStateProperty;
         private SerializedProperty _NumbersPerStateProperty;
 
+        private readonly static GUIContent _editorTitle = new("Laura's Parameter Compressor Settings");
+        private GUIStyle _titleStyle;
+        private GUILayoutOption _editorTitleHeight;
+
         public void OnEnable() {
+            _titleStyle = new(EditorStyles.boldLabel);
+            _titleStyle.fontSize *= 2;
+            _editorTitleHeight = GUILayout.Height(_titleStyle.CalcSize(_editorTitle).y);
+
             _ExcludedPropertyNamesProperty = serializedObject.FindProperty("ExcludedPropertyNames");
             _ExcludedPropertyNamePrefixesProperty = serializedObject.FindProperty("ExcludedPropertyNamePrefixes");
             _ExcludedPropertyNameSuffixesProperty = serializedObject.FindProperty("ExcludedPropertyNameSuffixes");
+
             _ExcludeVRCFTProperty = serializedObject.FindProperty("ExcludeVRCFT");
+
             _ExcludeBoolsProperty = serializedObject.FindProperty("ExcludeBools");
             _ExcludeIntsProperty = serializedObject.FindProperty("ExcludeInts");
             _ExcludeFloatsProperty = serializedObject.FindProperty("ExcludeFloats");
+
             _BoolsPerStateProperty = serializedObject.FindProperty("BoolsPerState");
             _NumbersPerStateProperty = serializedObject.FindProperty("NumbersPerState");
         }
@@ -33,14 +48,16 @@ namespace ParamComp.Editor.Components
         public override void OnInspectorGUI() {
             serializedObject.Update();
 
-            EditorGUILayout.Space();
-            EditorGUILayout.LabelField("Laura's Parameter Compressor Settings");
+            EditorGUILayout.LabelField(_editorTitle, _titleStyle, _editorTitleHeight);
             EditorGUILayout.HelpBox(
-                "This component requires VRCFury or Modular Avatar to be in the project, if you wish to test in in the editor.\r\n\r\n" +
-                "Without these it'll only work on a test build or avatar upload.",
-                MessageType.Info);
+                "For this compressor to work, this component is required to be placed on the root-object of your avatar, this is the object that also has the `Avatar Descriptor` component.\r\n\r\n" +
+                "While this tool doesn't have any hard dependencies, using `VRCFury` is recommended for best testing results.\r\n\r\n" +
+                "This component requires `VRCFury` or `Modular Avatar` to be in the project, if you wish to test in in the editor. Without either of these it will only work on a test build or avatar upload.",
+                MessageType.None
+            );
 
             EditorGUILayout.Space();
+            EditorGUILayout.LabelField("Parameter Name Exclusions", EditorStyles.boldLabel);
             EditorGUILayout.LabelField("Excluded Property Names");
             EditorGUILayout.PropertyField(_ExcludedPropertyNamesProperty, new GUIContent(string.Empty,
                 "These parameters will be excluded from the compressor. Keep in mind that any VRChat default parameters are excluded by default."
@@ -55,11 +72,13 @@ namespace ParamComp.Editor.Components
             ), true);
 
             EditorGUILayout.Space();
+            EditorGUILayout.LabelField("Package Specific Exclusions", EditorStyles.boldLabel);
             EditorGUILayout.PropertyField(_ExcludeVRCFTProperty, new GUIContent("Exclude VRCFT parameters",
                 "Exclude important VRCFaceTracking (Jerry's Templates) parameters from the compressor."
             ), true);
 
             EditorGUILayout.Space();
+            EditorGUILayout.LabelField("Parameter Type Exclusions", EditorStyles.boldLabel);
             EditorGUILayout.PropertyField(_ExcludeBoolsProperty, new GUIContent("Exclude Bools",
                 "Exclude all Boolean parameters from the compressor."
             ), true);
@@ -71,6 +90,7 @@ namespace ParamComp.Editor.Components
             ), true);
 
             EditorGUILayout.Space();
+            EditorGUILayout.LabelField("Output Settings", EditorStyles.boldLabel);
             EditorGUILayout.PropertyField(_BoolsPerStateProperty, new GUIContent("Bools Per State",
                 "Number of Booleans to sync per state."
             ), true);
--- Packages/com.laurarozier.paramcomp/Runtime/Components/ParamCompSettings.cs
+++ Packages/com.laurarozier.paramcomp/Runtime/Components/ParamCompSettings.cs
@@ -6,13 +6,18 @@ namespace ParamComp.Runtime.Components
     [AddComponentMenu("LauraRozier/ParamComp Settings")]
     public class ParamCompSettings : MonoBehaviour
     {
+        // Parameter Name Exclusions
         public string[] ExcludedPropertyNames = Array.Empty<string>();
         public string[] ExcludedPropertyNamePrefixes = Array.Empty<string>();
         public string[] ExcludedPropertyNameSuffixes = Array.Empty<string>();
+        // Package Specific Exclusions
         public bool ExcludeVRCFT = true;
+        // Parameter Type Exclusions
         public bool ExcludeBools = false;
         public bool ExcludeInts = false;
         public bool ExcludeFloats = false;
+
+        // Output Settings
         [Range(8, 32)]
         public int BoolsPerState = 8;
         [Range(1, 8)]
